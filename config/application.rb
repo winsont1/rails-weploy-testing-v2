d46a347e0189c05d9ac8cdb589fbe6b2de49768f
Winson Tan <winsont89@gmail.com>
2019-04-27 23:08:04 +1000

    Docs: Configured config/application.rb file to disable Timezone conversion, and added new Timesheet Form

--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,8 @@ module RailsWeployTestingV2
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.active_record.default_timezone = :local
+    config.active_record.time_zone_aware_attributes = false
   end
 end
+
